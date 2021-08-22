679c1bec2820a14c2e5e91ae259561a55bca5de6
tuasegun <tuase360@gmail.com>
2021-08-22 11:26:37 +0000

    initial commit

---
+++ src/components/pages/Signup/Signup.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjThree } from "./Data";
+
+function Signup() {
+  return (
+    <>
+      
+      
+      <HeroSection {...homeObjThree} />
+      
+      
+    </>
+  );
+}
+
+export default Signup;
